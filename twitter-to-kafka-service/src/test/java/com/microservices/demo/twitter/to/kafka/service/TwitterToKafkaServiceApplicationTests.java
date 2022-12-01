48521888761c8422ca1d687539a4a46dde64de5d
lenz <lenz.90@gmail.com>
2022-12-01 00:20:47 -0500

    adding config kafka model

---
+++ twitter-to-kafka-service/src/test/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.microservices.demo.twitter.to.kafka.service;
+
+import com.microservices.demo.twitter.to.kafka.services.TwitterToKafkaServiceApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TwitterToKafkaServiceApplication.class)
+public class TwitterToKafkaServiceApplicationTests {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
