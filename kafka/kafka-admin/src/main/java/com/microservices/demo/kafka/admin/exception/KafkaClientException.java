57d3b59dbdc8b0d435aa85e2a8213082e4253a79
lenz <lenz.90@gmail.com>
2022-12-05 23:16:03 -0500

    adding config kafka admin

---
+++ kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/exception/KafkaClientException.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.kafka.admin.exception;
+
+public class KafkaClientException extends RuntimeException {
+    public KafkaClientException(){
+
+    }
+    public KafkaClientException (String message) {
+
+    }
+    public KafkaClientException (String message, Throwable cause) {
+
+    }
+}
