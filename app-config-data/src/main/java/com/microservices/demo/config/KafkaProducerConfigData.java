725c5348a5b04590b4c233be0132ed30b3a43bfa
lenz <lenz.90@gmail.com>
2022-12-07 01:08:04 -0500

    adding config kafka producer

---
+++ app-config-data/src/main/java/com/microservices/demo/config/KafkaProducerConfigData.java
@@ -0,0 +1,21 @@
+package com.microservices.demo.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "kafka-producer-config")
+public class KafkaProducerConfigData {
+    private String keySerializerClass;
+    private String valuesSerializerClass;
+    private String compressionType;
+    private String acks;
+    private Integer batchSize;
+    private Integer batchSizeBoostFactor;
+    private Integer lingerMs;
+    private Integer requestTimeMs;
+    private Integer retryCount;
+
+}
